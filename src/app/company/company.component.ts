9ecb0629bc3df1e3e2151b36688ebf1f1c7eed8b
Bhushan-Mali <malib475@gmail.com>
2023-02-27 16:30:45 +0530

    extra functionality added company component

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-company',
@@ -10,13 +11,46 @@ export class CompanyComponent implements OnInit {
   EMAIL_PATTERN="^[_A-Aa-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[_A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
   MOBILE_PATTERN="/((\+*)((0[ -]*)*|((91 )*))((\d{12})+|(\d{10})+))|\d{5}([- ]*)\d{6}/";
 
-  state:any[]=[]
-  dist:any[]=[]
-  cat:any[]=[]
+  state:any[]=[ {name:'Andhra Pradesh'},{name:'Arunachal Pradesh'},{name:'Assam'},{name:'Bihar'},{name:'Chhattisgarh'},{name:'Goa'},{name:'Gujrat'},
+        {name:'Haryana'},{name:'Himachal Pradesh'},{name:'Jharkhand'},{name:'Karnataka'},{name:'Kerala'},{name:'Madhya Pradesh'}, {name:'Maharashtra'}, 
+        {name:'Manipur'},{name:'Meghalaya'},{name:'Mizoram'},{name:'Nagaland'},{name:'Odisha'},{name:'Punjab'},{name:'Rajasthan'},
+        {name:'Sikkim'},{name:'Tamil Nadu'},{name:'Telangana'},{name:'Tripura'},{name:'Uttarakhand'},{name:'Uttar Pradesh'},{name:'West Bengal'} 
+      ]
 
-  constructor() { }
+  // dist:any[]=[]
+
+  cat:any[]=[{name:'OPC'}, {name:'PVT'}, {name:'LLP'},{name:'GOV'},{name:'PVL'}
+      ]
+
+      // post:any[]=[];
+
+      // selectedItem:string='';
+      // selectedValue:string='';
+
+  constructor(private service:HttpService) { }
 
   ngOnInit(): void {
   }
 
+  // selectChange(event:any){
+  //   this.selectedItem=event.target.value;
+  // }
+
+  // selectChangeHandler(event:any){
+  //   this.selectedValue=event.target.value;
+  // }
+
+   onSend(mycomany:any, myaddress:any, mydate:any, mysts:any, mydis:any, mycity:any, myweb:any,
+          mycategory:any, mycomment:any, myfname:any, myccon:any, myemail:any, mycco:any, memail:any){
+     
+        let obj={
+          name:mycomany, address:myaddress, establishmentdate:mydate, state:mysts, district:mydis,
+          city:mycity, website:myweb, category:mycategory, comment:mycomment, hrfullName:myfname,
+          ccontact:myccon, hrgmail:myemail, hrcontact:mycco, cgmail:memail
+        }
+
+        this.service.PostData(obj).subscribe((response)=>{
+          console.log(response);
+        })
+   }
 }
